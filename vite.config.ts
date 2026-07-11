fa279235be0dd671ee424aa5afda0a0ff4ef1028
Михаил <etozhenerk@yandex.ru>
2026-07-12 02:32:56 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({mode}) => ({
+  base: mode === 'github-pages' ? '/DnD/' : '/',
   plugins: [react()],
   server: {
     host: '127.0.0.1',
@@ -11,4 +12,4 @@ export default defineConfig({
     host: '127.0.0.1',
     port: 4173,
   },
-});
+}));
